bd85c64096e4d39edae046ec54170164fecf7d89
sharatd <sharat.lakers24@gmail.com>
2023-01-09 21:01:02 -0600

    add create robot function

--- generate.py
+++ generate.py
@@ -18,3 +18,8 @@ def Create_World():
     pyrosim.Start_SDF("world.sdf")
     pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
     pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[x, y, z], size=[length, width, height])
+    pyrosim.End()
