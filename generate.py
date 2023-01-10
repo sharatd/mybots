bf03333017026310b64cb970eae452f99bf625f8
sharatd <sharat.lakers24@gmail.com>
2023-01-09 22:56:05 -0600

    Link 0 Link 1 Link 2

--- generate.py
+++ generate.py
@@ -13,9 +13,12 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Link0", pos=[x, y, z], size=[length, width, height])
+    pyrosim.Send_Cube(name="Link1", pos=[x, y, z], size=[length, width, height])
+    pyrosim.Send_Cube(name="Link2", pos=[x, y, z], size=[length, width, height])
     pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , 
     type = "revolute", position = [0,0,1])
-    pyrosim.Send_Cube(name="Link1", pos=[x, y, z], size=[length, width, height])
+    pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , 
+    type = "revolute", position = [0,0,1])
     pyrosim.End()
 
 Create_World()
