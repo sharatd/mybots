1a6fd365feb10afea435995cb76784c58e7a240f
sharatd <sharat.lakers24@gmail.com>
2023-01-09 20:15:04 -0600

    add another box

--- generate.py
+++ generate.py
@@ -2,6 +2,7 @@ import pyrosim.pyrosim as pyrosim
 
 length, width, height = 1, 1, 1
 x, y, z = 0, 0, 0.5
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+pyrosim.Send_Cube(name="Box2", pos=[x, y, z], size=[length, width, height])
 pyrosim.End()
