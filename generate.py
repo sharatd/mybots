008bf7a39ca4aecfce1a5da758ad39c6c4504de1
sharatd <sharat.lakers24@gmail.com>
2023-01-09 20:56:49 -0600

    Create World function

--- generate.py
+++ generate.py
@@ -4,12 +4,17 @@ length, width, height = 1, 1, 1
 x, y, z = 0, 0, 0.5
 x2, y2, z2 = 1, 0, 1.5
 
-pyrosim.Start_SDF("world.sdf")
-pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+#pyrosim.Start_SDF("world.sdf")
+#pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
 #for k in range(5):
 #    for j in range(5):
 #        for i in range(8):
 #            pyrosim.Send_Cube(name="Box"+str(i), pos=[x+j, y+k, z+i], size=[length*0.9**i, width*0.9**i, height*0.9**i])
 #pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
 #pyrosim.Send_Cube(name="Box2", pos=[x2, y2, z2], size=[length, width, height])
-pyrosim.End()
+#pyrosim.End()
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+    pyrosim.End()
