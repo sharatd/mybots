5c52c5647fb152d0f9018ac256d796ee30286c6a
sharatd <sharat.lakers24@gmail.com>
2023-01-09 20:30:43 -0600

    block tower

--- generate.py
+++ generate.py
@@ -5,6 +5,8 @@ x, y, z = 0, 0, 0.5
 x2, y2, z2 = 1, 0, 1.5
 
 pyrosim.Start_SDF("boxes.sdf")
-pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
-pyrosim.Send_Cube(name="Box2", pos=[x2, y2, z2], size=[length, width, height])
+for i in range(10):
+    pyrosim.Send_Cube(name="Box"+str(i), pos=[x, y, z+i], size=[length, width, height])
+#pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+#pyrosim.Send_Cube(name="Box2", pos=[x2, y2, z2], size=[length, width, height])
 pyrosim.End()
