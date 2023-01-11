0c9ff366e25ba7181095bccaa1c67d2cd6228564
sharatd <sharat.lakers24@gmail.com>
2023-01-11 11:46:57 -0600

    fixed part e

--- generate.py
+++ generate.py
@@ -12,19 +12,27 @@ def Create_World():
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Link0", pos=[x, y, z], size=[length, width, height])
-    pyrosim.Send_Cube(name="Link1", pos=[x, y, z], size=[length, width, height])
-    pyrosim.Send_Cube(name="Link2", pos=[x, y, z], size=[length, width, height])
-    pyrosim.Send_Cube(name="Link3", pos=[0, 0.5, 0], size=[length, width, height])
-    pyrosim.Send_Cube(name="Link4", pos=[0, 0.5, 0], size=[length, width, height])
+    pyrosim.Send_Cube(name="Link0", pos=[1.5, 0, 1.5], size=[length, width, height])
+    pyrosim.Send_Cube(name="Link1", pos=[0.5, 0, -0.5], size=[length, width, height])
+    pyrosim.Send_Cube(name="Link2", pos=[-0.5, 0, -0.5], size=[length, width, height])
     pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , 
-    type = "revolute", position = [0,0,1])
-    pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , 
-    type = "revolute", position = [0,0,1])
-    pyrosim.Send_Joint( name = "Link2_Link3" , parent= "Link2" , child = "Link3" , 
-    type = "revolute", position = [0,0,1])
-    pyrosim.Send_Joint( name = "Link3_Link4" , parent= "Link3" , child = "Link4" , 
-    type = "revolute", position = [0,0.5,0.5])
+    type = "revolute", position = [2, 0, 1])
+    pyrosim.Send_Joint( name = "Link0_Link2" , parent= "Link0" , child = "Link2" , 
+    type = "revolute", position = [1,0,1])
+    
+    # pyrosim.Send_Cube(name="Link0", pos=[x, y, z], size=[length, width, height])
+    # pyrosim.Send_Cube(name="Link1", pos=[x, y, z], size=[length, width, height])
+    # pyrosim.Send_Cube(name="Link2", pos=[x, y, z], size=[length, width, height])
+    # pyrosim.Send_Cube(name="Link3", pos=[0, 0.5, 0], size=[length, width, height])
+    # pyrosim.Send_Cube(name="Link4", pos=[0, 0.5, 0], size=[length, width, height])
+    # pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , 
+    # type = "revolute", position = [0,0,1])
+    # pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , 
+    # type = "revolute", position = [0,0,1])
+    # pyrosim.Send_Joint( name = "Link2_Link3" , parent= "Link2" , child = "Link3" , 
+    # type = "revolute", position = [0,0,1])
+    # pyrosim.Send_Joint( name = "Link3_Link4" , parent= "Link3" , child = "Link4" , 
+    # type = "revolute", position = [0,0.5,0.5])
     pyrosim.End()
 
 Create_World()
