f17ef5102b71f391a8c2dbe0f9c05672256aaa89
sharatd <sharat.lakers24@gmail.com>
2023-01-16 20:47:29 -0600

    setting constants

---
+++ constants.py
@@ -0,0 +1,13 @@
+import numpy
+
+
+amplitude_backleg = numpy.pi/4
+frequency_backleg = 5
+phaseOffset_backleg = numpy.pi/4
+
+amplitude_frontleg = numpy.pi/4
+frequency_frontleg = 5
+phaseOffset_frontleg = 0
+
+vectorSize = 1000
+maxForce = 500
